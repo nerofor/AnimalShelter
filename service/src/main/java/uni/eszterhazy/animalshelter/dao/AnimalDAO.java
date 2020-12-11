809ae7df8c940e2b75fbd4bbbfc040c71013d674
Gergő Soós <gergosoos4@gmail.com>
2020-12-11 12:56:49 +0100

    Added service modul
    Created AnimalDao class and defined and modified some services. And added an exception to method.

--- service/src/main/java/uni/eszterhazy/animalshelter/dao/AnimalDAO.java
+++ service/src/main/java/uni/eszterhazy/animalshelter/dao/AnimalDAO.java
@@ -1,14 +1,15 @@
 package uni.eszterhazy.animalshelter.dao;
 
+import uni.eszterhazy.animalshelter.exception.AnimalAlreadyAdded;
 import uni.eszterhazy.animalshelter.model.Animal;
 import uni.eszterhazy.animalshelter.model.Type;
 
 import java.util.Collection;
 
 public interface AnimalDAO {
-    void createAnimal(Animal animal);
+    void createAnimal(Animal animal) throws AnimalAlreadyAdded;
     Collection<Animal> readAllAnimal();
-    Animal readAnimal();
+    Animal readAnimal(String id);
     void updateAnimal(Animal animal);
     void deleteAnimal(Animal animal);
     Collection<Animal> readAllAnimalOfType(Type type); //ez már opcionális
